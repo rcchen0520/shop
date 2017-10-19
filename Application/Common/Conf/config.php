b8d62c9888c589267b44b663fec73d0cd98ee310
rcchen0520 <794106825@qq.com>
2017-10-20 00:17:18 +0800

    修改登录功能

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'rc_shop',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'rc_',    // 数据库表前缀
+    'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+    /*************** 修改I函数底层过滤时使用的函数 **********/
+    'DEFAULT_FILTER' => 'trim.removeXSS',
+    /*********** MD5复杂化的KEY ********/
+    'MD5_KEY' => '3D0FFE5291E42782F694FACFF526C22C',
 );
--- index.php
+++ index.php
@@ -20,7 +20,16 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
 
+//定义后台样式路径
+define('__ADMIN_CSS__','/Public/Admin/Styles');
+//定义后台JS路径
+define('__ADMIN_JS__','/Public/Admin/Js');
+//定义后台图片路径
+define('__ADMIN_IMG__','/Public/Admin/Images');
+
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
+
+
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
